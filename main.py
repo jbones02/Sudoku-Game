ebf2ddfd5270d714f08d8e54acb4ebbeb46cce9c
Jaryd <jaryd02@gmail.com>
2023-10-04 23:53:32 -0700

    Renamed Board class to Puzzle

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import board
+import game
+import puzzle
+
+sudoku_game = game.Game()
+sudoku_game.play()
+
+'''
 board1 = board.Board([[None, 7, None, None, 1, 4, None, None, None],
                       [6, 1, 2, None, 9, 5, 3, 8, None],
                       [3, None, 4, None, 6 ,8, 9, 7, 1],
@@ -30,3 +36,4 @@ board3 = board.Board([[7, 8, None, 4, None, None, 1, 2, None],
 board1.print()
 board1.solve()
 board1.print()
+'''
