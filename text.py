cd9bb55d6a2e44e31771b747d73d070dc7ab9c0a
Jaryd <jaryd02@gmail.com>
2023-10-06 00:26:12 -0700

    Started implementation of Button and Start_menu classes

--- text.py
+++ text.py
@@ -1,16 +1,17 @@
 import pygame
+import rgb
 
 class Text:
-    def __init__(self, text, font_file, size, x, y, WIN):
+    def __init__(self, text_input, font_file, size, x, y, WIN):
         self.__font = pygame.font.Font(font_file, size)
-        self.__BLACK = (0, 0, 0)
-        self.__text = self.__font.render(text, True, self.__BLACK)
+        self.__text_input = text_input
+        self.__text = self.__font.render(self.__text_input, True, rgb.BLACK)
         self.__x = x
         self.__y = y
         self.__WIN = WIN
         self.__text_rect = self.__text.get_rect()
+        self.__text_rect.center = (self.__x, self.__y)
 
     def draw(self):
-        self.__text_rect.center = (self.__x, self.__y)
         self.__WIN.blit(self.__text, self.__text_rect)
     
